093ec32689c265cef6494c5167e7be2a74f9bdb3
nekketsuuu <nekketsuuu@users.noreply.github.com>
2018-02-18 01:15:17 +0900

    CI: use brew to install OPAM on osx

--- travis/osx-before-script.sh
+++ travis/osx-before-script.sh
@@ -1,8 +1,6 @@
 # Download and install OPAM
 brew update > /dev/null
-brew install aspcud
-wget -O /usr/local/bin/opam https://github.com/ocaml/opam/releases/download/1.2.2/opam-1.2.2-x86_64-Darwin
-chmod +x /usr/local/bin/opam
+brew install aspcud opam
 opam init --compiler="${OCAML_VERSION}"
 eval `opam config env`
 opam install depext
