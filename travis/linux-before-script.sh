90d595bb0bbcd5ea6fe849394682a59bf3381997
nekketsuuu <nekketsuuu@users.noreply.github.com>
2018-03-02 15:07:20 +0900

    CI: use .travis-opam.sh in order to install XQuartz correctly

--- travis/linux-before-script.sh
+++ travis/linux-before-script.sh
@@ -2,7 +2,7 @@ DOCKERFILE="./travis/Dockerfile.template"
 
 sed -i "s/%distro%/${DISTRO}/g"               "${DOCKERFILE}"
 sed -i "s/%ocaml_version%/${OCAML_VERSION}/g" "${DOCKERFILE}"
-sed -i "s/%opambuildtest%/${OPAMBUILDTEST}/g" "${DOCKERFILE}"
+sed -i "s/%opambuildtest%/${TESTS}/g" "${DOCKERFILE}"
 sed -i "s/%package%/${PACKAGE}/g"             "${DOCKERFILE}"
 cat "${DOCKERFILE}"
 docker build --file "${DOCKERFILE}" --tag satysfi/satysfi:travis .
